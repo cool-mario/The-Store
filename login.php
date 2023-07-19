615e0491e2f09bd60639d84f988271b06af988ae
Aidan Chan <50786617+cool-mario@users.noreply.github.com>
2023-07-19 16:09:33 -0700

    cooler login page

--- login.php
+++ login.php
@@ -3,41 +3,34 @@
 <head>
   <title>  Log In   </title>
   <meta http-equiv="Content-Type" content="text/html;charset=utf-8">
-  <style>
-    body{
-        text-align: center;
-    }
-    #loginBox{
-    width:500px;
-    height: 400px;
-    margin: 0 auto;
-    border: solid 5px black;
-    border-radius: 5px;
-    }
-  </style>
+  <link rel="stylesheet" href="style.css">
+    <style>
+        form {
+            outline: 5px solid #03a283;
+            padding: 1.5% 4% 1.5% 4%;
+            border-radius: 10px;
+            line-height: 2;
+        }
+    </style>
 </head>
   <body>
-    <!-- Header -->
-    <h1>Store Name</h1>
-    <br>
-    <!-- Greeting -->
-    <h3>Please enter your login info </h3>
-    <!-- Credentials Input -->
-    <form action="store.php" method="post">
-        <div id="loginBox">
-            <div>
-            <label for="userInput1">Please enter your username</label>
-            <input id="userInput1" type="text" name="userName"></input>
-            </div>
-            <br>
-            <div>
-            <label for="userInput2">Please enter your password</label>
-            <input id="userInput2" type="text" name="password"></input>
-            </div>
-            <br>
-            <button type="submit">Enter</button>
+      <!-- Header -->
+      <h1>Store Name</h1>
+      <br>
+      <!-- Greeting -->
+      <h3>Please enter your login info </h3>
+      <!-- Credentials Input -->
+      <form action="store.php" method="post">
 
-        </div>
-    </form>
+        <label for="userInput1">Please enter your username</label>
+        <input id="userInput1" type="text" name="userName"></input>
+        <br>
+        <label for="userInput2">Please enter your password</label>
+        <input id="userInput2" type="password" name="password"></input>
+        <br>
+        <input type="submit" value="Log in">
+
+
+      </form>
   </body>
 </html>
