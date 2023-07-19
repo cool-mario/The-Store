e87a3c0e0eb29b431789f3b7e2cd6171c584b436
AdityaC2006 <82692095+AdityaC2006@users.noreply.github.com>
2023-07-19 16:16:06 -0700

    Update test.sql
    Added password system to users

--- test.sql
+++ test.sql
@@ -23,5 +23,17 @@ VALUES
     (1, 'Apples', 5),
     (1, 'Mangoes', 5),
     (1, 'Pencils', 5),
-    (1, 'Warheads', 5),
+    (1, 'Warheads', 5), 
     (1, 'Muffin Stumps', 5);
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `uName` text,
+  `role` boolean,
+  `hashpass` text
+);
+INSERT INTO `users`
+	(`uName`,`role`,`hashpass`)
+VALUES
+  ('testAdmin','1','$2y$10$QhCPlzi9.DndB2UDn3B7NusCvMsm2DcPY6VQO8BaGuCdc8iobEEM6'),
+  ('testUser','0','$2y$10$QhCPlzi9.DndB2UDn3B7NusCvMsm2DcPY6VQO8BaGuCdc8iobEEM6')
+  ;
