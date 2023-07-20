1bc60a1616f7c4a8665a79ccbaa28778532ee613
AdityaC2006 <82692095+AdityaC2006@users.noreply.github.com>
2023-07-20 16:19:25 -0700

    Adding functionality to buttons

--- test.sql
+++ test.sql
@@ -1,45 +1,112 @@
--- store
-CREATE TABLE IF NOT EXISTS `store` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `name` text,
-  `location` text,
-  `storeID` int,
-  PRIMARY KEY (`id`)
-);
-INSERT INTO `store`
-	(`name`,`location`,`storeID`)
-VALUES
-    ('Store 1','Lordran', 1);
+<?php
+session_start();
+require_once "config.php";
+$_SESSION["uName"] = $_POST["userName"];
+$_SESSION["pass"] = $_POST["password"];
+password_hash($_SESSION["pass"], PASSWORD_DEFAULT);
 
--- items
-CREATE TABLE IF NOT EXISTS `items` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `storeID` int,
-  `name` text,
-  `price` int,
-  `desc` text,
-  PRIMARY KEY (`id`)
-);
-INSERT INTO `items`
-	(`storeID`,`name`, `price`,`desc`)
-VALUES
-    (1, 'Apples', 5,'A normal apple. Very healthy'),
-    (1, 'Mangoes', 5, 'A very tasty mango!'),
-    (1, 'Pencils', 5, 'A very tasty pencil!'),
-    (1, 'Warheads', 5, 'Not for recreational use'), 
-    (1, 'Muffin Stumps', 5, 'Not for consumption');
 
--- users
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `uName` text,
-  `role` boolean,
-  `hashpass` text,
-  PRIMARY KEY (`id`)
-);
-INSERT INTO `users`
-	(`uName`,`role`,`hashpass`)
-VALUES
-  ('testAdmin','1','$2y$10$QhCPlzi9.DndB2UDn3B7NusCvMsm2DcPY6VQO8BaGuCdc8iobEEM6'),
-  ('testUser','0','$2y$10$QhCPlzi9.DndB2UDn3B7NusCvMsm2DcPY6VQO8BaGuCdc8iobEEM6')
-  ;
+
+
+// Password checking
+$dbh = new PDO(DB_DSN, DB_USER, DB_PASSWORD);
+$sth = $dbh->prepare("SELECT hashpass FROM `users` WHERE uName = :enteredName");
+$sth->bindValue(":enteredName",$_SESSION["uName"]);
+$sth->execute();
+$pass = $sth->fetch();
+
+if (!$pass){
+  header( "Location: login.php?m=name"); // go back to sign in if name is wrong
+}
+else {
+  if(password_verify($_SESSION["pass"],$pass[0])){
+    echo "<p>logged in</p>";
+  }
+  else{
+    header( "Location: login.php?m=pass"); // go back to sign in if password is wrong
+  }
+}
+
+if(!isset($_SESSION["cart"])){
+  $_SESSION["cart"] = array();
+}
+?>
+
+<!DOCTYPE html>
+<html  lang=en-US>
+<head>
+  <title>  Store Page  </title>
+  <meta http-equiv="Content-Type" content="text/html;charset=utf-8">
+  <link rel="stylesheet" href="style.css">
+  <style>
+    body{
+        text-align: center;
+    }
+    #products table, th, td{
+        /* border: 2px solid black; */
+    }
+
+  </style>
+</head>
+  <body>
+    <h1>Store Home Page</h1>
+
+    <?php
+    echo "<h2>Welcome {$_SESSION['uName']}!!!</h2>";
+    ?>
+    <!-- List of products -->
+    <table id="products">
+    <?php
+    try {
+        // Getting PKM
+        $sth = $dbh->prepare("SELECT * FROM items"); 
+        $sth->execute();
+        $items = $sth->fetchAll(); 
+        //   Item Names
+        echo "<tr>";   
+        foreach($items as $item){
+          echo "<td>  " . $item["name"] . "  </td>";
+        }
+        echo "</tr>";
+        //   Item Descriptions
+        echo "<tr>";   
+        foreach($items as $item){
+          echo "<td>  " . $item["desc"] . "  </td>";
+        }
+        echo "</tr>";
+        //   Item Pricce
+        echo "<tr>";   
+        foreach($items as $item){
+          echo "<td>  " . $item["price"] . "  </td>";
+        }
+        echo "</tr>";
+        //   Button to add to cart
+        echo "<tr>";   
+        foreach($items as $item){
+          echo "<td>"; 
+          echo "<form action='cart.php' method='post'>";
+          echo "<select>";
+          echo "<option value='". $item["id"] ."' class='hide'>an option</option>";
+          echo "</select>";
+          echo "<button type='submit'>Add to Cart</button>";
+          echo "</form>";
+          echo "</td>";
+        }
+        echo "</tr>";
+
+
+        
+      }
+      catch (PDOException $e) {
+          echo "<p>Error connecting to database!</p>";
+          echo $e->getMessage();
+      }
+    ?>
+    </table>
+
+  </body>
+</html>
+
+
+  </body>
+</html>
