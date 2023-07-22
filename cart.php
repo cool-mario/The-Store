abf0bdb697660620edfb8fe197c328cb2d7574f0
Aidan Chan <50786617+cool-mario@users.noreply.github.com>
2023-07-21 19:08:06 -0700

    now adds item to cart in session

--- cart.php
+++ cart.php
@@ -4,8 +4,33 @@ session_start();
 //     header("Location: signin.php"); // redirect to signin if not signed in
 //     die();
 // }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Added to shopping cart</title>
+</head>
+<body>
+<?php
+
 
 var_dump($_POST);
 var_dump($_SESSION["cart"]);
 
+if (isset($_POST["buy"])){
+    // add item to shopping cart
+    array_push($_SESSION["cart"], $_POST["buy"]);
+    header( "refresh:2; url=store.php");
+
+    echo "<h2>Added to your shopping cart!</h2>";
+}
+
 ?>
+    
+</body>
+</html>
+
+
