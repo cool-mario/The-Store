2243ca94fd07de7c2c14879a8100e0990a0867d0
AdityaC2006 <82692095+AdityaC2006@users.noreply.github.com>
2023-07-22 22:14:22 -0700

    Create ordered.php

---
+++ ordered.php
@@ -0,0 +1,12 @@
+<style>
+    body{
+        text-align:center;
+    }
+</style>
+
+
+<h1>Thank you for yur purchase!</h1>
+<br>
+<a href="store.php">Click here to continue shopping</a>
+<br>
+<a href="signout.php">Click here to continue log out</a>
