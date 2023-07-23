51c45fad0bd6565136be9c54ba1136a75279dd33
AdityaC2006 <82692095+AdityaC2006@users.noreply.github.com>
2023-07-23 13:42:43 -0700

    Create signup.php

---
+++ signup.php
@@ -0,0 +1,20 @@
+<style>
+    body{
+        text-align:center;
+    }
+</style>
+<h1>Register your account</h1>
+
+<form method="post" action="store.php">
+<label for="userInput1">Please enter a username: </label>
+<br>
+<input id="userInput1" type="text" name="inputName"></input>
+<br>
+<br>
+<label for="userInput2">Please enter a password: </label>
+<br>
+<input id="userInput2" type="password" name="inputPass"></input>
+<br>
+<input type="submit" value="register"></input>
+</form>
+
