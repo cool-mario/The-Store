a16c35317d3569a20f041544cbeee4b096b28b3e
Aidan Chan <50786617+cool-mario@users.noreply.github.com>
2023-07-24 14:01:47 -0700

    drop the user and cart tables

--- drop.php
+++ drop.php
@@ -10,6 +10,8 @@ try {
     $dbh = new PDO(DB_DSN, DB_USER, DB_PASSWORD);
     $dbh->exec('DROP TABLE IF EXISTS store; DROP TABLE IF EXISTS info;');
     $dbh->exec('DROP TABLE IF EXISTS items; DROP TABLE IF EXISTS info;');
+    $dbh->exec('DROP TABLE IF EXISTS users; DROP TABLE IF EXISTS info;');
+    $dbh->exec('DROP TABLE IF EXISTS cart;  DROP TABLE IF EXISTS info;');
     echo "<p>Successfully dropped databases</p>";
 }
 catch (PDOException $e) {
