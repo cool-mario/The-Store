cead8b53f5e2820a7692a1dcd2b49564b46b8513
Aidan Chan <50786617+cool-mario@users.noreply.github.com>
2023-07-23 20:13:38 -0700

    better css and link to login page

--- signup.php
+++ signup.php
@@ -5,20 +5,32 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
     <style>
-    body{
-        text-align:center;
-    }
+        body{
+            text-align:center;
+        }
+        form {
+            outline: 5px solid #03a283;
+            padding: 1.5% 4% 1.5% 4%;
+            border-radius: 10px;
+            line-height: 2;
+            margin-left: auto;
+            margin-right: auto;
+            width: 20vmax;
+        }
+        form > label, form > input, form > p {
+          font-size: 2vmin;
+        }
     </style>
     <title>Document</title>
 </head>
 <body>
-    <h1>Register your account</h1>
+    <h1>Store Name</h1>
+    <h2>Register your account</h2>
 
     <form method="post" action="store.php">
         <label for="userInput1">Please enter a username: </label>
         <br>
         <input id="userInput1" type="text" name="inputName"></input>
-        <br>
         <br>
         <label for="userInput2">Please enter a password: </label>
         <br>
@@ -26,6 +38,8 @@
         <br>
         <input type="submit" value="register"></input>
     </form>
+    <br>
+    <a href="login.php">Already have an account? Login</a>
     
 </body>
 </html>
