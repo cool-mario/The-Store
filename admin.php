adb6136280f8f22edcb11f989728816acb54646e
AdityaC2006 <82692095+AdityaC2006@users.noreply.github.com>
2023-07-25 00:08:24 -0700

    New files!

---
+++ admin.php
@@ -0,0 +1,72 @@
+<?php
+session_start();
+require_once "config.php"; 
+$dbh = new PDO(DB_DSN, DB_USER, DB_PASSWORD);
+?>
+<style>
+    body{
+        text-align:center;
+    }
+    table{
+        margin: 0px auto;
+    }
+</style>
+<h1>Welcome to the Admin Control Panel!</h1>
+<br>
+
+<form method="post" action="adminHandler.php">
+<!-- Select which item we change the price on -->
+<label for="inp1">Price Changing</label>
+<br>
+<select id="inp1"  name="chosenItem">
+<?php
+$sth = $dbh->prepare("SELECT * FROM `items`");
+$sth->execute(); 
+$itemsInfo = $sth->fetchAll();
+foreach($itemsInfo as $item){
+    echo "<option value='". $item["id"] ."'>". $item["name"] ."</option>";
+}
+?>
+</select>
+<br>
+<!-- Set a specific price on an item -->
+<label for="inp2">Set a price</label>
+<br>
+<input id="inp2"  name="chosenPrice" type="number">
+<br>
+<button type="submit">Change</button>
+</form>
+<?php
+if (isset($_GET['m']) && $_GET['m'] == "error"){
+    echo "<h1>You goofed up, donm't mess with the form :3</h1>";
+    } 
+?>
+
+<!-- Description changing -->
+<form method="post" action="adminHandler.php">
+<!-- Select which item we change the price on -->
+<label for="inp1">Description Changing</label>
+<br>
+<select id="inp1"  name="chosenItem2">
+<?php
+$sth = $dbh->prepare("SELECT * FROM `items`");
+$sth->execute(); 
+$itemsInfo = $sth->fetchAll();
+foreach($itemsInfo as $item){
+    echo "<option value='". $item["id"] ."'>". $item["name"] ."</option>";
+}
+?>
+</select>
+<br>
+<!-- Set a specific price on an item -->
+<label for="inp3">Set a description</label>
+<br>
+<input id="inp3" name="chosenDesc" type="text">
+<br>
+<button type="submit">Change</button>
+</form>
+<?php
+if (isset($_GET['m']) && $_GET['m'] == "error"){
+    echo "<h1>You goofed up, donm't mess with the form :3</h1>";
+    } 
+?>
---
+++ adminHandler.php
@@ -0,0 +1,74 @@
+<?php
+session_start();
+require_once "config.php"; 
+$dbh = new PDO(DB_DSN, DB_USER, DB_PASSWORD);
+$sth = $dbh->prepare("SELECT * FROM `items`");
+$sth->execute(); 
+$itemsInfo = $sth->fetchAll();
+
+if(isset($_POST["chosenItem"])){
+    if(filter_var($_POST["chosenItem"], FILTER_VALIDATE_INT) != false){
+        if($_POST["chosenItem"] > 0 and $_POST["chosenItem"] < 6){
+            // Validation for chosen item is complete
+            if(isset($_POST["chosenPrice"])){
+                if(filter_var($_POST["chosenPrice"], FILTER_VALIDATE_INT) != false){
+                    // Validation for chosen price is done
+                    echo "<p>Feet</p>";
+                    $sth = $dbh->prepare("UPDATE `items` SET `price` = :givenPrice WHERE id = :givenID");
+                    $sth->bindValue(':givenPrice',$_POST["chosenPrice"]);
+                    $sth->bindValue(':givenID',$_POST["chosenItem"] );
+                    $sth->execute(); 
+                }
+                else{
+                    header("Location: admin.php?m=error")
+                }
+            }      
+            else{
+                header("Location: admin.php?m=error")
+            }
+        }
+        else{
+            header("Location: admin.php?m=error")
+        }
+    }
+    else{
+        header("Location: admin.php?m=error")
+    }
+}
+else{
+    header("Location: admin.php?m=error")
+}
+if(isset($_POST["chosenItem2"])){
+    if(filter_var($_POST["chosenItem2"], FILTER_VALIDATE_INT) != false){
+        if($_POST["chosenItem2"] > 0 and $_POST["chosenItem2"] < 6){
+            // Validation for chosen item is complete
+            if(isset($_POST["chosenDesc"])){
+                    // Validation for chosen price is done
+                    echo "<p>Feet</p>";
+                    $sth = $dbh->prepare("UPDATE `items` SET `desc` = :givenPrice WHERE id = :givenID");
+                    $sth->bindValue(':givenPrice',$_POST["chosenDesc"]);
+                    $sth->bindValue(':givenID',$_POST["chosenItem2"] );
+                    $sth->execute(); 
+            }   
+            else{
+                header("Location: admin.php?m=error")
+            }
+        }
+        else{
+            header("Location: admin.php?m=error")
+        }
+    }
+    else{
+        header("Location: admin.php?m=error")
+    }
+}
+else{
+    header("Location: admin.php?m=error")
+}
+
+
+
+
+
+
+?>
