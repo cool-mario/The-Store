64e7a11e43b3c80f6f9ad62bdc816402149db998
AdityaC2006 <82692095+AdityaC2006@users.noreply.github.com>
2023-07-25 15:27:05 -0700

    Body Tags :0

--- ordered.php
+++ ordered.php
@@ -6,7 +6,19 @@
 
 
 <h1>Thank you for yur purchase!</h1>
-<br>
+
+
+<!DOCTYPE html>
+<html  lang=en-US>
+<head>
+  <title>  Ordered!  </title>
+  <meta http-equiv="Content-Type" content="text/html;charset=utf-8">
+</head>
+  <body>
+  <br>
 <a href="store.php">Click here to continue shopping</a>
 <br>
 <a href="signout.php">Click here to continue log out</a>
+  </body>
+</html>
+
