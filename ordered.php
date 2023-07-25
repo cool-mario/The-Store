c92a87c3cc19bd05a7378cc44f98877a3d5a1a24
Aidan Chan <50786617+cool-mario@users.noreply.github.com>
2023-07-25 16:00:48 -0700

    empty the session and database cart

--- ordered.php
+++ ordered.php
@@ -1,24 +1,63 @@
-<style>
-    body{
-        text-align:center;
-    }
-</style>
+<?php
+session_start();
+require_once "config.php";
+$dbh = new PDO(DB_DSN, DB_USER, DB_PASSWORD);
 
 
-<h1>Thank you for yur purchase!</h1>
+
+
+
+?>
 
 
 <!DOCTYPE html>
-<html  lang=en-US>
+<html lang="en">
 <head>
-  <title>  Ordered!  </title>
-  <meta http-equiv="Content-Type" content="text/html;charset=utf-8">
+    <meta http-equiv="Content-Type" content="text/html;charset=utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <style>
+        body{
+            text-align:center;
+        }
+    </style>
+
+    <title>Document</title>
 </head>
-  <body>
-  <br>
-<a href="store.php">Click here to continue shopping</a>
+<body>
+
+<?php
+
+if (isset($_SESSION["userID"])){
+    // delete all items owned by the user in the data
+    $sth = $dbh->prepare("DELETE FROM cart 
+    WHERE cart.user_id = :userID
+    ");
+    $sth->bindValue(":userID", $_SESSION["userID"]);
+    $sth->execute();
+
+    // empty the session array
+    $_SESSION["cart"] = array();
+
+    echo "<h1>Thank you for your purchase!</h1>";
+
+} else {
+    echo "an error occured...........";
+}
+
+
+?>
+
+
+
 <br>
-<a href="signout.php">Click here to continue log out</a>
-  </body>
+<a href="store.php"><button>Click here to shop for more stuff</button></a>
+<br><br>
+<a href="signout.php"><button>Click here to Log Out</button></a>
+    
+</body>
 </html>
+
+
+
 
