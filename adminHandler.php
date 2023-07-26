f7392189bc7d3a1e5e14322f923c8af43dc183c6
AdityaC2006 <82692095+AdityaC2006@users.noreply.github.com>
2023-07-25 22:09:46 -0700

    Added deletion handler

--- adminHandler.php
+++ adminHandler.php
@@ -9,9 +9,13 @@ $sth = $dbh->prepare("SELECT * FROM `items`");
 $sth->execute(); 
 $itemsInfo = $sth->fetchAll();
 
+$sth = $dbh->prepare("SELECT id FROM `items` ORDER BY `id` DESC LIMIT 1");
+$sth->execute(); 
+$itemNum = $sth->fetch();
+
 if(isset($_POST["chosenItem"])){
     if(filter_var($_POST["chosenItem"], FILTER_VALIDATE_INT) != false){
-        if($_POST["chosenItem"] > 0 and $_POST["chosenItem"] < 6){
+        if($_POST["chosenItem"] > 0 and $_POST["chosenItem"] < $itemNum){
             // Validation for chosen item is complete
             if(isset($_POST["chosenPrice"])){
                 if(filter_var($_POST["chosenPrice"], FILTER_VALIDATE_INT) != false){
@@ -41,7 +45,7 @@ if(isset($_POST["chosenItem"])){
 }
 if(isset($_POST["chosenItem2"])){
     if(filter_var($_POST["chosenItem2"], FILTER_VALIDATE_INT) != false){
-        if($_POST["chosenItem2"] > 0 and $_POST["chosenItem2"] < 6){
+        if($_POST["chosenItem2"] > 0 and $_POST["chosenItem2"] < $itemNum){
             // Validation for chosen item is complete
             if(isset($_POST["chosenDesc"])){
                     // Validation for chosen price is done
@@ -64,5 +68,22 @@ if(isset($_POST["chosenItem2"])){
         header("Location: admin.php?m=error");
     }
 }
-
+if(isset($_POST["chosenItem3"])){
+    if(filter_var($_POST["chosenItem3"], FILTER_VALIDATE_INT) != false){
+        if($_POST["chosenItem3"] > 0 and $_POST["chosenItem3"] < $itemNum){
+// Validation of item selected is complete
+        $sth = $dbh->prepare("DELETE FROM `items` WHERE items.id = :idD;");
+        $sth->bindValue(':idD',$_POST["chosenItem3"]);
+        $sth->execute(); 
+        header("Location: admin.php?m=success");
+        }
+    else{
+        header("Location: admin.php?m=error"); 
+    }
+    }
+    else{
+        header("Location: admin.php?m=error"); 
+    }
+}
 ?>
+
